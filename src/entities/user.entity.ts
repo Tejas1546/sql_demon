e6e6bc0b23c52c531db42c453f1dbdc5e5abb45e
Tejas Shetty <jshettytejas@gmail.com>
2026-04-11 15:54:38 +0530

    initial

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,26 +1,28 @@
-import { BaseEntitiy, type IBaseEntity } from "./base.entitiy.js";
-import { Table } from "./table.decorator.js";
+import { BaseEntity, type IBaseEntity } from "./base.entity.js";
+import { Table, TABLE_METADATA_KEY } from "./table.decorator.js";
 
-interface IUser extends IBaseEntity {
-  name: string;
-  address: string;
-  dob: Date;
-  email: string;
+
+export interface IUser extends IBaseEntity {
+    name: string;
+    address: string;
+    dob: Date;
+    email: string;
 }
 
-@Table("users")
-export class User extends BaseEntitiy implements IUser {
-  private static TABLE_NAME = "users";
-  name: string;
-  address: string;
-  dob: Date;
-  email: string;
+@Table('users')
+export class User extends BaseEntity implements IUser {
 
-  constructor(user: IUser) {
-    super(user);
-    this.name = user.name;
-    this.address = user.address;
-    this.dob = user.dob;
-    this.email = user.email;
-  }
+    name: string;
+    address: string;
+    dob: Date;
+    email: string;
+
+    constructor(user: IUser) {
+        super(user);
+        this.name = user.name;
+        this.address = user.address;
+        this.dob = user.dob;
+        this.email = user.email;
+    }
+    
 }
